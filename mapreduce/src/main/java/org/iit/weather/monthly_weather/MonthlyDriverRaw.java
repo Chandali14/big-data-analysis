3e49edbfba939d74058b459684ca3f68e9a25f1c
Chandali14 <anshumalakodithuwakkuu@gmail.com>
2025-12-18 18:09:59 +0530

    add MonthlyDriverRaw

---
+++ mapreduce/src/main/java/org/iit/weather/monthly_weather/MonthlyDriverRaw.java
@@ -0,0 +1,41 @@
+package org.iit.weather.monthly_weather;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.net.URI;
+
+public class MonthlyDriverRaw {
+
+    public static void main(String[] args) throws Exception {
+
+        if (args.length != 3) {
+            System.err.println("Usage: MonthlyDriverRaw <weatherData> <locationData> <output>");
+            System.exit(2);
+        }
+
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "District Monthly Weather Aggregation (Raw)");
+
+        job.setJarByClass(MonthlyDriverRaw.class);
+
+        job.setMapperClass(MonthlyMapper.class);
+        job.setReducerClass(MonthlyReducerRaw.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        // Add locationData.csv to distributed cache with alias
+        job.addCacheFile(new URI(args[1] + "#locationData.csv"));
+
+        TextInputFormat.addInputPath(job, new Path(args[0]));
+        TextOutputFormat.setOutputPath(job, new Path(args[2]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
